30cc699d65919ac589e745444c7b1e9bae9dad31
Avaneesh <pathakavaneesh@gmail.com>
2020-02-15 20:17:04 +0530

    Added dice roll animation on shaking the iPhone

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var diceArray = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -84,20 +86,52 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                         z: hitResult.worldTransform.columns.3.z
                     )
+                    
+                    diceArray.append(diceNode)
 
                     sceneView.scene.rootNode.addChildNode(diceNode)
                     
-                    let randomX = Float((arc4random_uniform(4) + 1)) * (Float.pi/2)
-                    //        let randomY = Double((arc4random_uniform(10) + 11)) * (Double.pi/2)
-                    let randomZ = Float((arc4random_uniform(4) + 1)) * (Float.pi/2)
-                    
-                    diceNode.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 5), y: 0, z: CGFloat(randomZ * 5), duration: 0.5))
+                    roll(dice: diceNode)
 
                 }
                 
             }
             
         }
+    }
+    
+    func rollAll() {
+        
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+        
+    }
+    
+    func roll(dice: SCNNode) {
+        
+        let randomX = Float((arc4random_uniform(4) + 1)) * (Float.pi/2)
+        //        let randomY = Double((arc4random_uniform(10) + 11)) * (Double.pi/2)
+        let randomZ = Float((arc4random_uniform(4) + 1)) * (Float.pi/2)
+        
+        dice.runAction(SCNAction.rotateBy(
+            x: CGFloat(randomX * 5),
+            y: 0,
+            z: CGFloat(randomZ * 5),
+            duration: 0.5)
+        )
+        
+    }
+    
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
     }
     
     // MARK: - ARSCNViewDelegate
